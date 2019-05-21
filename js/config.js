b59196b9e0d89feb579b2d9ed6a28fdadf6fce4a
jesusgom28 <jesusgomezm9728@gmail.com>
2019-05-21 16:14:21 -0700

    new stuff

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiamVzdXNnb20yOCIsImEiOiJjanY5c3E3cXgwZDlsNDN0ZG15ZG9paGQ5In0.I4LePrQbU_mmlT6rnp8phA";
+const API_KEY = "pk.eyJ1IjoiamVzdXNnb20yOCIsImEiOiJjanZla2k0dDgwNnV1M3lwNDJpNGFxNXB0In0.PnpqwCU7ABXzWGM0AAgceQ";
