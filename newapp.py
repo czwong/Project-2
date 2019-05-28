d7c1a97dcaa0d0094c98ecec0f74c4b514dea918
jesusgom28 <jesusgomezm9728@gmail.com>
2019-05-28 10:39:54 -0700

    app

--- newapp.py
+++ newapp.py
@@ -1,30 +1,3 @@
-# import datetime as dt
-# import numpy as np
-# import pandas as pd
-# import datetime as dt
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
-# from flask import Flask, jsonify
-
-# engine = create_engine("sqlite:///tickets.sqlite")
-# Base = automap_base()
-# Base.prepare(engine, reflect=True)
-
-# Station = Base.classes.station
-# Measurements = Base.classes.measurements
-
-# session = Session(engine)
-
-# app = Flask(__name__)
-
-# @app.route("https://api.seatgeek.com/2/events?")
-# def event():
-#     """Return a list of events"""
-#     return (
-#         f"venues available:"
-#     )
 import flask
 from flask import Flask, render_template, request, jsonify
 from flask_mysqldb import MySQL
@@ -99,4 +72,3 @@ return jsonify(results)
 
 if __name__ == '__main__':
     app.run()
-
