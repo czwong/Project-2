2e404252c6e607e7faddf8a203a532a173ab7863
Chris Wong <chriswong90012@yahoo.com>
2019-05-28 12:16:57 -0700

    adding app

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-# import datetime as dt
-# import numpy as np
-# import pandas as pd
-# import datetime as dt
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
-# from flask import Flask, jsonify
+import os
 
-# engine = create_engine("sqlite:///tickets.sqlite")
-# Base = automap_base()
-# Base.prepare(engine, reflect=True)
+import pandas as pd
+import numpy as np
 
-# Station = Base.classes.station
-# Measurements = Base.classes.measurements
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
-# session = Session(engine)
-
-# app = Flask(__name__)
-
-# @app.route("https://api.seatgeek.com/2/events?")
-# def event():
-#     """Return a list of events"""
-#     return (
-#         f"venues available:"
-#     )
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+#################################################
+# Database Setup
+#################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+db = SQLAlchemy(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
